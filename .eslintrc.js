bfc1906d6f0a199083d5fa82f246bed23d98fced
Nick Rayburn <52075362+nrayburn-tech@users.noreply.github.com>
2022-08-01 03:50:23 -0500

    Add eslint, prettier, and typescript Remove autochangelog Move index.js into lib folder

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: {
+    node: true,
+    es2016: true,
+  },
+  rules: {
+    '@typescript-eslint/ban-ts-comment': ['off'],
+  },
+};
