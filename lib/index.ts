a7b809eb7291a91a6451d0ea258799ad37d08db1
Nick Rayburn <52075362+nrayburn-tech@users.noreply.github.com>
2022-09-18 18:06:19 -0500

    Add a todo comment with some possible PRs to investigate implementing

--- lib/index.ts
+++ lib/index.ts
@@ -28,3 +28,12 @@ export const createProxyServer = createProxy;
  * @see {createProxy}
  */
 export const createServer = createProxy;
+
+// TODO: PRs/Bugs to review and possibly migrate from original repo.
+//  crash when 'options.target' is an object instead a string - https://github.com/http-party/node-http-proxy/pull/1600
+//  [fix] do not append XHeader values - https://github.com/http-party/node-http-proxy/pull/1581
+//  Add lookup option to override dns lookups - https://github.com/http-party/node-http-proxy/pull/1574
+//  Fix 'aborted' detection on Node v15.5.0+ - https://github.com/http-party/node-http-proxy/pull/1559
+//  Fix websocket socket close before response - https://github.com/http-party/node-http-proxy/pull/1552
+//  Add follow-redirects options support - https://github.com/http-party/node-http-proxy/pull/1533
+//  Fix handling of client disconnect so that econnreset event is emitted instead of error - https://github.com/http-party/node-http-proxy/pull/1542
